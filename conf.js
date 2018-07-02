378a7263a6a1e87d322c56aa4214474f23763e27
temikng <temikng@gmail.com>
2018-07-02 22:08:47 +0300

    Added project config files

---
+++ conf.js
@@ -0,0 +1,46 @@
+/*jslint node: true */
+"use strict";
+exports.port = null;
+//exports.myUrl = 'wss://mydomain.com/bb';
+exports.bServeAsHub = false;
+exports.bLight = false;
+
+exports.storage = 'sqlite';
+
+// TOR is recommended.  If you don't run TOR, please comment the next two lines
+//exports.socksHost = '127.0.0.1';
+//exports.socksPort = 9050;
+
+exports.hub = 'byteball.org/bb';
+exports.deviceName = 'Username attestation bot';
+exports.permanent_pairing_secret = '0000';
+exports.control_addresses = [''];
+exports.payout_address = 'WHERE THE MONEY CAN BE SENT TO';
+
+exports.bIgnoreUnpairRequests = true;
+exports.bSingleAddress = false;
+exports.bStaticChangeAddress = true;
+exports.KEYS_FILENAME = 'keys.json';
+
+//email
+exports.useSmtp = false;
+exports.admin_email = '';
+exports.from_email = '';
+
+// witnessing
+exports.bRunWitness = false;
+exports.THRESHOLD_DISTANCE = 20;
+exports.MIN_AVAILABLE_WITNESSINGS = 100;
+
+// Reddit karma rewards
+exports.arrUsernameLengthPriceInBytes = [
+  {threshold: 3, price: 150},
+  {threshold: 4, price: 100},
+  {threshold: 6, price: 50},
+  {threshold: 7, price: 10}
+];
+
+// set this in conf.json
+exports.salt = null;
+
+exports.webPort = 8080;
